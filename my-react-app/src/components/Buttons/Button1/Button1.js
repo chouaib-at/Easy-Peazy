290fa9be72501bd1b28644bdc89e2667d38d15c0
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-03-08 23:36:52 +0100

    s

--- my-react-app/src/components/Buttons/Button1/Button1.js
+++ my-react-app/src/components/Buttons/Button1/Button1.js
@@ -3,8 +3,8 @@ import "./Button1.css";
 
 const Button1 = () => {
   const handleButtonClick = () => {
-    // Replace 'your-github-url' with the actual GitHub URL you want to navigate to
-    window.location.href = "https://github.com/your-username/your-repository";
+    window.location.href =
+      "https://github.com/chouaib-at/Easy-Peazy/tree/master/my-react-app/src/components/Buttons/Button1";
   };
 
   return (
